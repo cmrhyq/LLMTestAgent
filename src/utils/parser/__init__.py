7242ba60332a8f807e41f426ff495e1ff7ab63f1
AlanHuang <1678336204@qq.com>
2026-04-22 17:43:13 +0800

    新增openapi 文档解析器

---
+++ src/utils/parser/__init__.py
@@ -0,0 +1,15 @@
+"""
+输入解析器包
+
+提供用户原始输入（自定义 JSON）与标准 OpenAPI 文档的解析能力。
+"""
+
+from src.utils.parser.input_parser import InputParser, parse_input
+from src.utils.parser.openapi_parser import OpenAPIParser, parse_openapi
+
+__all__ = [
+    "InputParser",
+    "parse_input",
+    "OpenAPIParser",
+    "parse_openapi",
+]
