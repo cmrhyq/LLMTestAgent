2da96a0f88ee1f2fa69756e5e209f554889cf2db
AlanHuang <1678336204@qq.com>
2026-05-06 16:52:53 +0800

    更新input example文件和unit test case

--- tests/test_parser_api.py
+++ tests/test_parser_api.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from src.utils.parser import parse_openapi
+from src.utils.parser import OpenAPIParser
 
 if __name__ == '__main__':
     project_root = Path(__file__).parent.parent
-    input_file = project_root / "input" / "httpbin_service.json"
-    parsed_date = parse_openapi(input_file)
-    for item in parsed_date[1]:
+    input_file = project_root / "input" / "test.json"
+    parser = OpenAPIParser(input_file)
+    for item in parser.endpoints:
         print(item)
